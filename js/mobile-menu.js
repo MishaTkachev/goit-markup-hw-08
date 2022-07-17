98261df3c76c9ca15998bf76dd8846b3447802b9
MishaTkachev <99650019+MishaTkachev@users.noreply.github.com>
2022-07-17 18:39:57 +0300

    fixed check.svg

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -18,8 +18,6 @@
   openMenuBtn.addEventListener("click", toggleMenu);
   closeMenuBtn.addEventListener("click", toggleMenu);
 
-  // Закрываем мобильное меню на более широких экранах
-  // в случае изменения ориентации устройства.
   window.matchMedia("(min-width: 768px)").addEventListener("change", (e) => {
     if (!e.matches) return;
     mobileMenu.classList.remove("is-open");
@@ -27,18 +25,3 @@
     bodyScrollLock.enableBodyScroll(document.body);
   });
 })();
-
-// (() => {
-//   const menuBtnRef = document.querySelector("[data-menu-button]");
-//   const mobileMenuRef = document.querySelector("[data-menu]");
-
-//   menuBtnRef.addEventListener("click", () => {
-//     const expanded =
-//       menuBtnRef.getAttribute("aria-expanded") === "true" || false;
-
-//     menuBtnRef.classList.toggle("is-open");
-//     menuBtnRef.setAttribute("aria-expanded", !expanded);
-
-//     mobileMenuRef.classList.toggle("is-open");
-//   });
-// })();
